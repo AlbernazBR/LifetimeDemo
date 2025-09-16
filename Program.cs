5e84e020321b08c43f700b7bd5c55a714f04bcb3
SYLVIO ALBERNAZ MACHADO NETO <saneto2@latam.stefanini.com>
2025-09-16 17:51:19 -0300

    -

--- Program.cs
+++ Program.cs
@@ -1,12 +1,36 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Registro dos serviços
+// lifetimes
 builder.Services.AddTransient<TransientService>();
 builder.Services.AddScoped<ScopedService>();
 builder.Services.AddSingleton<SingletonService>();
 
 builder.Services.AddControllers();
+
+// -- Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Lifetime Demo API",
+        Version = "v1",
+        Description = "Demonstração de lifetimes: Transient, Scoped, Singleton"
+    });
+});
+
 var app = builder.Build();
 
+// -- Swagger UI (pode deixar sempre ligado ou só em Dev)
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Lifetime Demo API v1");
+    c.RoutePrefix = "swagger"; // URL: /swagger
+});
+
 app.MapControllers();
+
 app.Run();
